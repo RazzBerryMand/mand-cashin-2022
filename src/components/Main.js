86f074eb9cf134dce14f07951b4c51b8f68712e9
Mand Cashin <mandcashin@hotmail.co.uk>
2022-01-01 15:07:45 +0000

    fix: font sizes and paraphrase text

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,22 +6,22 @@ export const Main = () => {
     <main>
       <div className="content-box">
         <h2>About</h2>
-        <p>I am based in Manchester, UK. My pronouns are She/Her.</p>
+        <p>I live in Manchester, England. My pronouns are She/Her.</p>
       </div>
       <div className="content-box">
         <h2>Front End Development</h2>
         <p>
-          I have around 3 years commercial experience in HTML, CSS and JS. I
-          learnt to code at Northcoders in 2018 and have since worked both in
-          house and for digital agencies.
+          I have roughly 3 years of commercial HTML, CSS, and JS experience. In
+          2018, I learned to code at Northcoders and have since worked in-house
+          and for digital agencies.
         </p>
         <p>
-          My prefered tech stack is React with a focus on site performance and
-          slick looking UX. I also get excited about CRO, accessibility and
-          agile processes.
+          React is my preferred tech stack, with an emphasis on site performance
+          and a smooth user experience. CRO, accessibility, and agile processes
+          also excite me.
         </p>
         <p>
-          If you'd like to see examples of my work, you can take a look at my{" "}
+          Take a peek at my{" "}
           <a
             href="https://github.com/RazzBerryMand"
             target="_blank"
@@ -36,8 +36,8 @@ export const Main = () => {
             rel="noreferrer"
           >
             Codepen
-          </a>
-          .
+          </a>{" "}
+          if you want to see some examples of my work.
         </p>
       </div>
       <div className="content-box">
@@ -47,16 +47,16 @@ export const Main = () => {
           volunteer as a mentor at Code Your Future.
         </p>
         <p>
-          If you consider yourself to be of a minority background and are
-          thinking about a career in tech, you can message me on{" "}
+          If you are a member of a minority group and are considering a career
+          in tech, send me a message on{" "}
           <a
             href="https://www.linkedin.com/in/mandcashin/"
             target="_blank"
             rel="noreferrer"
           >
             LinkedIn
-          </a>{" "}
-          and I will offer my advice and point you in the direction of some free
+          </a>
+          . I will offer my advice and guide you in the direction of some free
           resources to get you started.
         </p>
       </div>

b6834b1c6f786e5eace80413892d0087fff1fb7e
Mand Cashin <mandcashin@hotmail.co.uk>
2022-01-20 16:50:06 +0000

    feat: add image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import profilePicture from "../assets/IMG_5630.JPG";
 
 export const Header = () => {
   return (
     <header>
+      <img src={profilePicture} alt="Mand Cashin" />
       <h1>Mand Cashin</h1>
       <p>
         Want to say hi? Reach me on{" "}

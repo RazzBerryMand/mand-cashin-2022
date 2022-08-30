40bac95b946551adef3c4187077e9f96e2e32868
Mand Cashin <mandcashin@hotmail.co.uk>
2022-08-30 12:16:18 +0100

    feat: update header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import profilePicture from "../assets/IMG_3010.jpg";
+import profilePicture from "../assets/IMG_9957.jpg";
 
 export const Header = () => {
   return (
